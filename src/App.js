e8f28a4b40a476807d0a95dc6ceb7faa21a7053c
Habib Maksoud <habibmaksoud@gmail.com>
2023-06-14 21:02:19 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+
+import Skills from "./components/Skills";
+
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className="text-gray-400 bg-gray-900 body-font">
+        <Navbar />
+        <About />
+        {/* <Projects /> */}
+        <Skills />
+        <Contact />
+    </main>
+    );
 }
 
 export default App;
---
+++ src/components/About.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function About () {
+    return (
+        <section id="about" className="text-gray-400 bg-gray-900 body-font">
+            <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+                <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 md:mb-0 mb-10">
+                    <img className="object-cover object-center rounded" alt="hero" src='./assets/images/hero.jpg' />
+                </div>
+                <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Hi, I'm <span className="text-indigo-400">Habib</span>.
+                        <br className="hidden lg:inline-block" />I love to build amazing apps.
+                    </h1>
+                    <p className="mb-8 leading-relaxed">I am a full stack web developer with a passion for creating and learning. I have experience with HTML, CSS, JavaScript, Node.js, Express.js, MySQL, MongoDB, React.js, and more. I am a graduate of the UCLA Extension DEVx Coding Bootcamp. I am currently seeking a position as a full stack web developer.</p>
+                    <div className="flex justify-center">
+                        <a href="#contact" className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Work With Me</a>
+                        <a href="#projects" className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">See My Past Work</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};

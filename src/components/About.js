2fad607069c04c68a2924e0b887011b2acab4d4e
Habib Maksoud <habibmaksoud@gmail.com>
2023-06-21 21:05:41 -0700

    added resume button or download

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Container, Row, Col, Image } from "react-bootstrap";
 import Contact from "./Contact";
 import profilePic from "../../src/assets/IMG_0676.jpg";
+import resumePDF from "../../src/assets/resume-web.pdf";
 import "../about.css";
 
 export default function About() {
@@ -10,7 +11,12 @@ export default function About() {
       <Container>
         <Row className="align-items-center">
           <Col xs={4}>
-            <Image className="profile-pic" src={profilePic} alt="Profile Picture" fluid />
+            <Image
+              className="profile-pic"
+              src={profilePic}
+              alt="Profile Picture"
+              fluid
+            />
           </Col>
           <Col xs={8}>
             <h2 className="mb-4">About Me</h2>
@@ -30,12 +36,19 @@ export default function About() {
             </p>
             <p>
               When I'm not coding, you can find me exploring new technologies,
-              playing soccer, or geeking out around cool cars. I am a big cooking enthusiast and love to try new recipes. I also enjoy traveling and exploring new places.
+              playing soccer, or geeking out around cool cars. I am a big
+              cooking enthusiast and love to try new recipes. I also enjoy
+              traveling and exploring new places.
             </p>
             <p>
-              I am currently looking for a full-time position as a web developer. If you have any questions or would like to get in touch, please feel free to contact me using the button below.
+              I am currently looking for a full-time position as a web
+              developer. If you have any questions or would like to get in
+              touch, please feel free to contact me using the button below.
             </p>
             <Contact />
+            <a href={resumePDF} target="_blank" rel="noopener noreferrer">
+              View/Download Resume Here
+            </a>
           </Col>
         </Row>
       </Container>
